c41c7653aeb7b56be6b340826077e7fd81ed2110
busami-star <victorbusami1@gmail.com>
2024-12-04 19:19:33 +0300

    first  commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 
 function Main() {
-    const ingredients = ["ginger", "tumeric","chicken"]
-    const ingredientsList = ingredients.map(ingredient => (<li key={ingredient}>{ingredient}</li>))
-    function submitForm(event){
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsList = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function submitForm(event) {
+      
         event.preventDefault()
-        console.log("submitted")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-  return (
-    <main>
-      <form onSubmit={submitForm} className='ingredient-form'>
-        <input 
-        type='text'
-        placeholder='e.g. olive oil'
-        aria-label='Add ingredient'
-        name='ingredient'
-        />
-        <button>Add Ingredient</button>
-      </form>
-      <ul>{ingredientsList}</ul>
-    </main>
-  );
+
+    return (
+        <main>
+            <form onSubmit={submitForm} className="ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. cumin"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsList}
+            </ul>
+        </main>
+    )
 }
 
 
-
 export default Main;
+
+
+
